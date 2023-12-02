5e319e94da6e235bd4ac89f876099b6a56a5cc7b
Ali Abdallah <itsaliabdallah@gmail.com>
2023-12-02 00:43:15 +0000

    add preset difficulties

--- src/app/_components/GameSettings.tsx
+++ src/app/_components/GameSettings.tsx
@@ -16,6 +16,7 @@ export default function GameSettings({
 }) {
   const [wordLength, setWordLength] = useState(5);
   const [wordAmount, setWordAmount] = useState(25);
+  const [gameMode, setGameMode] = useState("EASY");
 
   const gameStart = async () => {
     const data = await getWords(wordLength, wordAmount);
@@ -24,25 +25,72 @@ export default function GameSettings({
   };
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <div className="flex justify-center items-center">
-        <div className="text-white flex flex-col justify-center items-center">
-          WORD LENGTH
-          <CustomNumberInput
-            value={wordLength}
-            setValue={setWordLength}
-            type="length"
-          />
-        </div>
-        <div className="text-white flex flex-col justify-center items-center">
-          WORD AMOUNT
-          <CustomNumberInput
-            value={wordAmount}
-            setValue={setWordAmount}
-            type="amount"
-          />
+    <div className="flex flex-col justify-center items-center min-h-screen max-w-screen">
+      <div>
+        <button
+          onClick={() => {
+            setWordAmount(15);
+            setWordLength(4);
+            setGameMode("EASY");
+          }}
+          className="text-white border-white border-4 p-2 rounded-2xl m-4"
+        >
+          EASY
+        </button>
+        <button
+          onClick={() => {
+            setWordAmount(25);
+            setWordLength(7);
+            setGameMode("MEDIUM");
+          }}
+          className="text-white border-white border-4 p-2 rounded-2xl m-4"
+        >
+          MEDIUM
+        </button>
+        <button
+          onClick={() => {
+            setWordAmount(40);
+            setWordLength(9);
+            setGameMode("HARD");
+          }}
+          className="text-white border-white border-4 p-2 rounded-2xl m-4"
+        >
+          HARD
+        </button>
+        <button
+          onClick={() => {
+            setWordAmount(25);
+            setWordLength(5);
+            setGameMode("CUSTOM");
+          }}
+          className="text-white border-white border-4 p-2 rounded-2xl m-4"
+        >
+          CUSTOM
+        </button>
+      </div>
+
+      <div className="flex flex-col justify-center items-center border-4 rounded-2xl p-4 m-6">
+        <div className="text-white text-4xl m-6">{gameMode}</div>
+        <div className="flex">
+          <div className="text-white flex flex-col justify-center items-center">
+            WORD LENGTH
+            <CustomNumberInput
+              value={wordLength}
+              setValue={setWordLength}
+              type="length"
+            />
+          </div>
+          <div className="text-white flex flex-col justify-center items-center">
+            WORD AMOUNT
+            <CustomNumberInput
+              value={wordAmount}
+              setValue={setWordAmount}
+              type="amount"
+            />
+          </div>
         </div>
       </div>
+
       <motion.button
         onClick={gameStart}
         className="text-white rounded-full border-8 border-white w-80 h-80 text-7xl justify-center my-20"
